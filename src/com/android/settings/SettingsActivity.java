f1668d03ed9b33b8f3b8a563649967c3c3f40827
beanstown106 <nbenis106@gmail.com>
2016-03-29 22:37:29 -0400

    Build JDCSettings into settings [2/3]
    Thanks to camcory for the manifest part
    
    Change-Id: I899c439f69d3e3931a929c7871d6764ae585854f

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -132,7 +132,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
-import com.android.settings.OptCMSettings;
+import com.jdc.settings.JDCSettings;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -248,6 +248,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             Settings.RoamingSettingsActivity.class.getName(),
             Settings.SimSettingsActivity.class.getName(),
             Settings.WirelessSettingsActivity.class.getName(),
+            //custom_section
+            JDCSettings.class.getName(),
             //device_section
             Settings.HomeSettingsActivity.class.getName(),
             Settings.SoundSettingsActivity.class.getName(),
@@ -367,6 +369,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             WifiInfo.class.getName(),
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
+            JDCSettings.class.getName(),
     };
 
 
