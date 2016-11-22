6e2edb01c32e221dd956b57c18e7e1ed4681e72e
Mattia D'Alleva <antaresone@antaresone.eu>
2016-11-22 22:35:17 +0100

    OptCMSettings: add missing import

--- src/org/jdcteam/settings/fragments/OptCMSettings.java
+++ src/org/jdcteam/settings/fragments/OptCMSettings.java
@@ -2,6 +2,8 @@ package org.jdcteam.settings.fragments;
 
 import android.os.Bundle;
 
+import com.android.settings.SettingsPreferenceFragment;
+
 public class OptCMSettings extends SettingsPreferenceFragment {
 		
 	@Override
